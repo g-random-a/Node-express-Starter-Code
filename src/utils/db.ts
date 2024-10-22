129c93982dc239bbeb475d98bdff285bc30cdc06
Yared02 <101086562+Yared02@users.noreply.github.com>
2024-10-22 09:58:17 -0700

    some updates

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/datacollection", {
+    await mongoose.connect(process.env.MONGO_DB_URI || "mongodb://localhost:27017/datacollection", {
     //   useNewUrlParser: true,
     //   useUnifiedTopology: true,
     });
