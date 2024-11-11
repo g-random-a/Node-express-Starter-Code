712b62e1abd5e2c3dcc8e239ef818f024d354de2
Bereket Tilahun <btilahun287@gmail.com>
2024-11-11 16:25:19 +0300

    feat/ci-cd setup

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,9 @@ app.use(cors()); // Enable All CORS Requests
 
 app.use(express.json());
 // setupSwagger(app); // Setup Swagger documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1/response/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use("/api", responseRoutes);
+app.use("/api/v1/response", responseRoutes);
 
 
 export default app;
